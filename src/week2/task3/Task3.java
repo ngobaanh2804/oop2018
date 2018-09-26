40fdccf5cb710d8aedd63e232471cd677b616b61
ngobaanh2804 <ngobaanh280499@gmail.com>
2018-09-26 11:30:26 +0700

    week2.2

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,51 @@
 package week2.task3;
 
 public class Task3 {
+    // TODO doi tung giao vien
+    class teacher{
+        String name;
+        int age;
+        String sex;
+        public String getName()
+        {
+            return this.name;
+        }
+        public void setName(String name) {
+            this.name=name;
+        }
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public String getSex()
+        {
+            return this.sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+        // TODO ten giao vien
+        public void Name()
+        {
+            System.out.println("ten toi la"+ this.name);
+        }
+        // TODO giao vien dang day hoc
+        public void Teach()
+        {
+            System.out.println("I am teaching");
+        }
+        // TODO mon hoc
+        public void Subject()
+        {
+            System.out.println("day mon toan");
+        }
+    }
+
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
