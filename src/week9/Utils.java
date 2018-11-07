65a9f9bc102d5ab731a5daec7a3a3560f09af415
ngobaanh2804 <ngobaanh280499@gmail.com>
2018-11-07 23:13:21 +0700

    week5

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,4 +1,35 @@
 package week9;
 
+import java.io.*;
 public class Utils {
+    public static String readContentFromFile(String path) throws IOException {
+        File file = new File(path);
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String string = bufferedReader.readLine();
+        bufferedReader.close();
+        return string;
+    }
+
+    public static void writeContentToFile(String path) throws IOException {
+        File file = new File(path);
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        //bufferedWriter.newLine();
+        bufferedWriter.write("baanh");
+        bufferedWriter.close();
+    }
+
+    public static File findFileByName(String folderPath, String fileName) {
+        File file = new File(folderPath);
+        if (file.isDirectory()) {
+            String[] list = file.list();
+            for (String string : list) {
+                if (string.equals(fileName)) {
+                    return new File(folderPath + "\\" + fileName);
+                }
+            }
+        }
+        return null;
+    }
 }
